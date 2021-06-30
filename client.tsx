731b38d31323877e0ea3c28e0290ea27cf21bc71
DESKTOP-KA70MJ9\ma <hihiduk119@gmail.com>
2021-06-30 14:28:16 +0900

    no message

--- client.tsx
+++ client.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
+import App from './layouts/App';
 
 render(
-   <div>test</div>, 
+   <App />, 
    document.querySelector('#app')
 )
