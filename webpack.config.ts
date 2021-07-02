ab93890a4ca65c55cf1c898338aa1e0414cbe0a2
DESKTOP-KA70MJ9\ma <hihiduk119@gmail.com>
2021-07-02 19:01:06 +0900

    no message

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,8 +1,8 @@
 import path from 'path';
-// import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import webpack from 'webpack';
-// import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
-// import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
@@ -41,14 +41,14 @@ const config: webpack.Configuration = {
                   '@babel/preset-react',
                   '@babel/preset-typescript',
                ],
-               // env: {
-               //    development: {
-               //       plugins: [['@emotion', { sourceMap: true }], require.resolve('react-refresh/babel')],
-               //    },
-               //    production: {
-               //       plugins: ['@emotion'],
-               //    },
-               // },
+               env: {
+                  development: {
+                     plugins: [['@emotion', { sourceMap: true }], require.resolve('react-refresh/babel')],
+                  },
+                  production: {
+                     plugins: ['@emotion'],
+                  },
+               },
             },
             exclude: path.join(__dirname, 'node_modules'),
          },
@@ -58,40 +58,43 @@ const config: webpack.Configuration = {
          },
       ],
    },
+   
    plugins: [
-      // new ForkTsCheckerWebpackPlugin({
-      //    async: false,
-      //    eslint: {
-      //      files: "./src/**/*",
-      //    },
-      // }),
+      new ForkTsCheckerWebpackPlugin({
+         async: false,
+         // eslint: {
+         //   files: "./src/**/*",
+         // },
+      }),
       new webpack.EnvironmentPlugin({ NODE_ENV: isDevelopment ? 'development' : 'production' }),
    ],
+
    output: {
       path: path.join(__dirname, 'dist'),
       filename: '[name].js',
       publicPath: '/dist/',
    },
-   // devServer: {
-   //    historyApiFallback: true, // react router
-   //    port: 3090,
-   //    publicPath: '/dist/',
-   //    proxy: {
-   //       '/api/': {
-   //          target: 'http://localhost:3095',
-   //          changeOrigin: true,
-   //       },
-   //    },
-   // },
+   
+   devServer: {
+      historyApiFallback: true, // react router
+      port: 3090,
+      publicPath: '/dist/',
+      // proxy: {
+      //    '/api/': {
+      //       target: 'http://localhost:3095',
+      //       changeOrigin: true,
+      //    },
+      // },
+   },
 };
 
 if (isDevelopment && config.plugins) {
-//   config.plugins.push(new webpack.HotModuleReplacementPlugin());
-//   config.plugins.push(new ReactRefreshWebpackPlugin());
+  config.plugins.push(new webpack.HotModuleReplacementPlugin());
+  config.plugins.push(new ReactRefreshWebpackPlugin());
 //   config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'server', openAnalyzer: true }));
 }
 if (!isDevelopment && config.plugins) {
-//   config.plugins.push(new webpack.LoaderOptionsPlugin({ minimize: true }));
+  config.plugins.push(new webpack.LoaderOptionsPlugin({ minimize: true }));
 //   config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'static' }));
 }
 
