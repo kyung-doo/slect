cd80ec75a42b874f968c321d150faa156141faaa
kyung-doo <hohomkd@naver.com>
2021-07-03 14:57:40 +0900

    df

--- layouts/App.tsx
+++ layouts/App.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Login from '@pages/Login';
+import SignUp from '@pages/SignUp';
 
 const App = () => {
    return (
